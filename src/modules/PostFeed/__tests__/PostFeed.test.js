c4c0577899fa5ed08e62a6761134cc9348e26718
Mike Carney <michaelrcarney@gmail.com>
2021-01-20 20:45:16 -0600

    Post Feed Tests

--- src/modules/PostFeed/__tests__/PostFeed.test.js
+++ src/modules/PostFeed/__tests__/PostFeed.test.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { render, cleanup, waitFor, screen} from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 
 import PostFeed from '../PostFeed';
 
